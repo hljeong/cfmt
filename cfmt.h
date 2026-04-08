8fd0b4685a924897e14a7ee8d65af17d32cc23d2
Ho Lyun Jeong <holyun.lucas.jeong@gmail.com>
2026-04-07 22:51:04 -0700

    implement alphabet soup

--- cfmt.h
+++ cfmt.h
@@ -14,6 +14,11 @@
 #define vemitf              cfmt_decorate(vemitf)
 #define emitf               cfmt_decorate(emitf)
 #define print               cfmt_decorate(print)
+#define vprint              cfmt_decorate(vprint)
+#define fprint              cfmt_decorate(fprint)
+#define vfprint             cfmt_decorate(vfprint)
+#define snprint             cfmt_decorate(snprint)
+#define vsnprint            cfmt_decorate(vsnprint)
 #endif
 
 
@@ -30,38 +35,44 @@
 
 typedef struct {
   void *self;
-  void (*emit)(void *self, const char *s);
+  int (*emit)(void *self, const char *s);
 } sink;
 
 typedef struct formatter formatter;
 struct formatter {
   const char *spec;
-  void (*fmt)(const sink s, va_list);
+  int (*fmt)(const sink s, va_list);
   formatter *next;
 };
 
 extern formatter FORMATTERS;
-formatter *add_formatter(const char *spec, void (*fmt)(const sink s, va_list));
+formatter *add_formatter(const char *spec, int (*fmt)(const sink s, va_list));
 
 const int OK             =  0;
-const int OVERFLOW       = -1;
-const int SINGLE_RBRACE  = -2;
-const int MISSING_RBRACE = -3;
-const int UNKNOWN_SPEC   = -4;
+const int OVERFLOW       = -727;
+const int SINGLE_RBRACE  = -728;
+const int MISSING_RBRACE = -729;
+const int UNKNOWN_SPEC   = -730;
 
 int vemitf(const sink s, const char *fmt, va_list ap);
-int emitf(const sink s, const char *fmt, ...);
+int emitf (const sink s, const char *fmt, ...);
 
-void print(const char *fmt, ...);
+int    print(const char *fmt, ...);
+int   vprint(const char *fmt, va_list ap);
+int   fprint(FILE *file, const char *fmt, ...);
+int  vfprint(FILE *file, const char *fmt, va_list ap);
+int  snprint(char *buf, const size_t size, const char *fmt, ...);
+int vsnprint(char *buf, const size_t size, const char *fmt, va_list ap);
 
 #endif
 
 // instantiate the implementation with #define CFMT_IMPL
+#define CFMT_IMPL
 #ifdef CFMT_IMPL
 
 formatter FORMATTERS = {};
 
-formatter *add_formatter(const char *spec, void (*fmt)(const sink s, va_list)) {
+formatter *add_formatter(const char *spec, int (*fmt)(const sink s, va_list)) {
   formatter *f = calloc(1, sizeof(formatter));
   f->spec = spec;
   f->fmt = fmt;
@@ -71,6 +82,7 @@ formatter *add_formatter(const char *spec, void (*fmt)(const sink s, va_list)) {
 
 int vemitf(const sink s, const char *fmt, va_list ap) {
   char buf[CFMT_SEG_BUF_LEN];
+  int total_len = 0;
 
   const char *seg = fmt;
   while (*fmt) {
@@ -88,21 +100,27 @@ int vemitf(const sink s, const char *fmt, va_list ap) {
 
         const int len = vsnprintf(buf, sizeof(buf), seg_fmt, ap);
         if (len >= sizeof(buf)) return OVERFLOW;
-        s.emit(s.self, buf);
+        const int ret = s.emit(s.self, buf);
+        if (ret < 0) return ret;
+        total_len += ret;
       }
     }
 
     if (c == '}') {
       // "}}" -> "}"
       if (*fmt++ != '}') return SINGLE_RBRACE;
-      s.emit(s.self, "}");
+      const int ret = s.emit(s.self, "}");
+      if (ret < 0) return ret;
+      total_len += ret;
       seg = fmt;
     }
 
     // "{{" -> "{"
     else if (*fmt == '{') {
       fmt++;
-      s.emit(s.self, "{");
+      const int ret = s.emit(s.self, "{");
+      if (ret < 0) return ret;
+      total_len += ret;
       seg = fmt;
     }
 
@@ -123,7 +141,9 @@ int vemitf(const sink s, const char *fmt, va_list ap) {
       formatter *f = FORMATTERS.next;
       while (f) {
         if (!strcmp(spec_buf, f->spec)) {
-          f->fmt(s, ap);
+          const int ret = f->fmt(s, ap);
+          if (ret < 0) return ret;
+          total_len += ret;
           break;
         }
         f = f->next;
@@ -138,10 +158,12 @@ int vemitf(const sink s, const char *fmt, va_list ap) {
   if (*seg) {
     const int len = vsnprintf(buf, sizeof(buf), seg, ap);
     if (len >= sizeof(buf)) return OVERFLOW;
-    s.emit(s.self, buf);
+    const int ret = s.emit(s.self, buf);
+    if (ret < 0) return ret;
+    total_len += ret;
   }
 
-  return 0;
+  return total_len;
 }
 
 int emitf(const sink s, const char *fmt, ...) {
@@ -152,15 +174,63 @@ int emitf(const sink s, const char *fmt, ...) {
   return ret;
 }
 
-void _emit_print(void *self, const char *s) {
-  printf("%s", s);
+typedef struct { FILE *file; } _cfmt_fprint_sink;
+typedef struct { char *buf; size_t n; } _cfmt_snprint_sink;
+
+static int _cfmt_print(void *self, const char *s) {
+  return printf("%s", s);
 }
 
-void print(const char *fmt, ...) {
+static int _cfmt_fprint(void *self, const char *s) {
+  return fprintf(((_cfmt_fprint_sink *) self)->file, "%s", s);
+}
+
+static int _cfmt_snprint(void *self, const char *s) {
+  _cfmt_snprint_sink *sink = self;
+  const int ret = snprintf(sink->buf, sink->n, "%s", s);
+  if (ret < 0) return ret;
+  const int advance = ret >= sink->n ? sink->n : ret;
+  sink->buf += advance;
+  sink->n -= advance;
+  return ret;
+}
+
+int vprint(const char *fmt, va_list ap) {
+  return vemitf((sink) { .emit = _cfmt_print }, fmt, ap);
+}
+
+int print(const char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
-  vemitf((sink) { .emit = _emit_print }, fmt, ap);
+  const int ret = vprint(fmt, ap);
   va_end(ap);
+  return ret;
+}
+
+int vfprint(FILE *file, const char *fmt, va_list ap) {
+  _cfmt_fprint_sink s = { .file = file };
+  return vemitf((sink) { .self = &s, .emit = _cfmt_fprint }, fmt, ap);
+}
+
+int fprint(FILE *file, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  const int ret = vfprint(file, fmt, ap);
+  va_end(ap);
+  return ret;
+}
+
+int vsnprint(char *buf, const size_t n, const char *fmt, va_list ap) {
+  _cfmt_snprint_sink s = { .buf = buf, .n = n };
+  return vemitf((sink) { .self = &s, .emit = _cfmt_snprint }, fmt, ap);
+}
+
+int snprint(char *buf, const size_t n, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  const int ret = vsnprintf(buf, n, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 #undef CFMT_IMPL
@@ -168,6 +238,12 @@ void print(const char *fmt, ...) {
 #endif
 
 
+#undef vsnprint
+#undef snprint
+#undef vfprint
+#undef fprint
+#undef vprint
+#undef print
 #undef emitf
 #undef vemitf
 #undef UNKNOWN_SPEC
